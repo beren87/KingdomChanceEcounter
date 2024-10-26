cffc865ff2dc1d741e5dd53bf1f75fc7286ab8fd
berenger ferguenis <berenger.ferguenis@g5-digital.com>
2024-10-26 11:22:33 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex items-center justify-center min-h-screen bg-blue-500 text-white'>
+      <h1 className='text-3xl font-bold'>
+        Mon jeu de cartes en React avec Tailwind CSS
+      </h1>
     </div>
   );
 }
